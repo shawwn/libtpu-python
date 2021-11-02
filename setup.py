d18346f9d925c7ad5d7f1343cb5039c13110cc80
Shawn Presser <shawnpresser@gmail.com>
2021-11-02 03:52:34 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['libtpu']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'libtpu',
+    'version': '0.1.0',
+    'description': 'A reference libtpu.so implementation for Python and JAX',
+    'long_description': '',
+    'author': 'Shawn Presser',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
